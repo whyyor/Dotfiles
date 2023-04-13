9f6c2b8fce36eccf2482a86f4a959d49eebaccc0
whyyor <whyyor@gmail.com>
2023-04-13 14:26:30 +0530

    add dashboard indentline and lualine, bug dashboard

---
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+require('indent_blankline').setup({
+  filetype_exclude = {
+    'dashboard',
+    'help',
+    'terminal',
+    'packer',
+    'lspinfo',
+    'TelescopePrompt',
+    'TelescopeResults',
+  },
+  buftype_exclude = {
+    'terminal',
+    'NvimTree',
+  },
+})
