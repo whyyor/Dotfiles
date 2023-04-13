6a30c4d17564404c65c41df94de8dcc18673928d
whyyor <whyyor@gmail.com>
2023-04-14 01:31:35 +0530

    change font size and add some lsp definations

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -23,7 +23,7 @@ require("lspconfig").jsonls.setup({
 	},
 })
 
--- Setup lspconfig.
+-- Null-ls.
 require("lspconfig").cssls.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 })
