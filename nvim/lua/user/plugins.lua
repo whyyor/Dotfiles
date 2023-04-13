8ab2ea81ca33809503ee01b457bf35d49688f645
whyyor <whyyor@gmail.com>
2023-04-13 14:44:06 +0530

    Add gitsigns and lazygit

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -54,7 +54,7 @@ use({
   end,
   })
 
--- Commenting support using n`gcc`
+-- Commenting support using 'n'`gcc`
 use('tpope/vim-commentary')
 
 -- Add, change, and delete surrounding text.
@@ -204,6 +204,27 @@ use({
     require('user/plugins/dashboard-nvim')
   end
 })
+
+-- Git integration.
+use({
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup({current_line_blame = true})
+    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+    vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+    vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+  end,
+})
+
+-- Git commands.
+use({
+'kdheepak/lazygit.nvim',
+  requires = 'kdheepak/lazygit.nvim',
+})
+
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
