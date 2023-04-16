1f4a98e54f2232d00716853764630f3576e8ecf9
whyyor <whyyor@gmail.com>
2023-04-16 15:44:02 +0530

    use custom ascii

--- nvim/lua/user/plugins/dashboard.lua
+++ nvim/lua/user/plugins/dashboard.lua
@@ -7,10 +7,12 @@ dashboard.setup({
 			"",
 			"",
 			"",
-			"  ⢸⣦⡈⠻⣿⣿⣿⣶⣄",
-			"    ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄",
-			"⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄",
-			"  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄",
+			"██╗    ██╗██╗  ██╗██╗   ██╗██╗   ██╗ ██████╗ ██████╗ ",
+			"██║    ██║██║  ██║╚██╗ ██╔╝╚██╗ ██╔╝██╔═══██╗██╔══██╗",
+			"██║ █╗ ██║███████║ ╚████╔╝  ╚████╔╝ ██║   ██║██████╔╝",
+			"██║███╗██║██╔══██║  ╚██╔╝    ╚██╔╝  ██║   ██║██╔══██╗",
+			"╚███╔███╔╝██║  ██║   ██║      ██║   ╚██████╔╝██║  ██║",
+			"╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝    ╚═════╝ ╚═╝  ╚═╝",
 			"",
 			"",
 			"",
@@ -19,27 +21,27 @@ dashboard.setup({
 		}, --your header
 		center = {
 			{
-				icon = "                   ",
+				icon = "                 ",
 				desc = "New file",
 				action = "enew",
 			},
 
 			{
-				icon = "                   ",
+				icon = "                 ",
 				shortcut = "SPC f",
 				desc = "Find file",
 				action = "Telescope find_files",
 			},
 
 			{
-				icon = "                   ",
+				icon = "                 ",
 				shortcut = "SPC h",
 				desc = "Recent files           ",
 				action = "Telescope oldfiles",
 			},
 
 			{
-				icon = "                   ",
+				icon = "                 ",
 				shortcut = "SPC g",
 				desc = "Find Word           ",
 				action = "Telescope live_grep",
@@ -49,7 +51,3 @@ dashboard.setup({
 	},
 })
 
-vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#6272a4' })
-vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
-vim.api.nvim_set_hl(0, 'DashboardShortcut', { fg = '#bd93f9' })
-vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#6272a4' })
