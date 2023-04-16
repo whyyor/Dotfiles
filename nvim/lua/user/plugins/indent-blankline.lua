af07496a40459ea313d8a5f08609e28c22d344cf
whyyor <whyyor@gmail.com>
2023-04-16 20:24:13 +0530

    add intent active lines, shift nvim tree to right

--- nvim/lua/user/plugins/indent-blankline.lua
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -1,15 +1,21 @@
-require('indent_blankline').setup({
-  filetype_exclude = {
-    'dashboard',
-    'help',
-    'terminal',
-    'packer',
-    'lspinfo',
-    'TelescopePrompt',
-    'TelescopeResults',
-  },
-  buftype_exclude = {
-    'terminal',
-    'NvimTree',
-  },
+require("indent_blankline").setup({
+	filetype_exclude = {
+		"dashboard",
+		"help",
+		"terminal",
+		"packer",
+		"lspinfo",
+		"TelescopePrompt",
+		"TelescopeResults",
+	},
+	buftype_exclude = {
+		"terminal",
+		"NvimTree",
+	},
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
 })
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,18 +1,24 @@
-require('nvim-tree').setup({
-  git = {
-    ignore = false,
-  },
-  renderer = {
-    group_empty = true,
-    icons = {
-      show = {
-        folder_arrow = false,
-      },
-    },
-    indent_markers = {
-      enable = true,
-    },
-  },
+require("nvim-tree").setup({
+	git = {
+		ignore = false,
+	},
+	renderer = {
+		group_empty = true,
+		icons = {
+			show = {
+				folder_arrow = false,
+			},
+		},
+		indent_markers = {
+			enable = true,
+		},
+	},
+	view = {
+		-- set NvimTree to be 30% of the screen width
+		width = 30,
+		-- position NvimTree on the right side of the screen
+		side = "right",
+	},
 })
 
-vim.keymap.set('n', '<Leader>n', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set("n", "<Leader>n", ":NvimTreeFindFileToggle<CR>")
