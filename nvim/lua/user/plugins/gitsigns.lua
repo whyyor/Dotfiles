58f22a9e136dd752142065c35032ff4d1836ece2
whyyor <whyyor@gmail.com>
2023-12-20 14:59:55 +0530

    UPDATE: gitsigns config

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 local colors = {
 	add = "#acd385",
-	delete = "#fd4485",
+	delete = "#f65a81",
 }
 
 require("gitsigns").setup({
@@ -12,21 +12,21 @@ require("gitsigns").setup({
 		changedelete = { text = "~" },
 		untracked = { text = "┆" },
 	},
-	signcolumn = true,
-	numhl = false,
-	linehl = false,
-	word_diff = false,
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
-		interval = 1000,
 		follow_files = true,
 	},
 	attach_to_untracked = true,
-	current_line_blame = true,
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = "eol",
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 		delay = 1000,
 		ignore_whitespace = false,
+		virt_text_priority = 100,
 	},
 	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
 	sign_priority = 6,
@@ -34,6 +34,7 @@ require("gitsigns").setup({
 	status_formatter = nil, -- Use default
 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
 	preview_config = {
+		-- Options passed to nvim_open_win
 		border = "single",
 		style = "minimal",
 		relative = "cursor",
