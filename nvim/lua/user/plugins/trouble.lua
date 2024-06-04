2e856e1c10534c519fa836e0f07374bf8fbbd5df
whyyor <whyyor@gmail.com>
2024-06-04 23:46:55 +0530

    FIX: trouble, UPDATE: Plugins

--- nvim/lua/user/plugins/trouble.lua
+++ nvim/lua/user/plugins/trouble.lua
@@ -7,20 +7,30 @@ trouble.setup({
 })
 
 vim.keymap.set("n", "<leader>pp", function()
-	require("trouble").toggle()
+	require("trouble").toggle("diagnostics")
 end)
-vim.keymap.set("n", "<leader>pw", function()
-	require("trouble").toggle("workspace_diagnostics")
-end)
-vim.keymap.set("n", "<leader>pf", function()
-	require("trouble").toggle("document_diagnostics")
-end)
-vim.keymap.set("n", "<leader>pq", function()
-	require("trouble").toggle("quickfix")
-end)
-vim.keymap.set("n", "<leader>pl", function()
-	require("trouble").toggle("loclist")
-end)
-vim.keymap.set("n", "gR", function()
-	require("trouble").toggle("lsp_references")
-end)
+
+
+-- Set Trouble background to transparent
+vim.cmd([[
+  highlight TroubleNormal guibg=none ctermbg=none
+  highlight TroubleText guibg=none ctermbg=none
+  highlight TroubleFile guibg=none ctermbg=none
+  highlight TroubleFoldIcon guibg=none ctermbg=none
+  highlight TroubleCount guibg=none ctermbg=none
+  highlight TroubleLocation guibg=none ctermbg=none
+  highlight TroubleIndent guibg=none ctermbg=none
+  highlight TroubleSignError guibg=none ctermbg=none
+  highlight TroubleSignWarning guibg=none ctermbg=none
+  highlight TroubleSignInformation guibg=none ctermbg=none
+  highlight TroubleSignHint guibg=none ctermbg=none
+  highlight TroublePreview guibg=none ctermbg=none
+  highlight TroubleCode guibg=none ctermbg=none
+  highlight TroubleSource guibg=none ctermbg=none
+
+  " Also set the background of the normal and normal float highlight groups to none
+  highlight Normal guibg=none ctermbg=none
+  highlight NormalNC guibg=none ctermbg=none
+  highlight NormalFloat guibg=none ctermbg=none
+  highlight NonText guibg=none ctermbg=none
+]])
