6f73cd2653ddc450e32315354903e7ea5d7b4c74
whyyor <whyyor@gmail.com>
2024-09-11 16:52:18 +0530

    UPDATE: markdown wiki styles
    UPDATE: opacity of background

--- nvim/lua/user/plugins/markdown.lua
+++ nvim/lua/user/plugins/markdown.lua
@@ -1,11 +1,4 @@
 require("markview").setup({
-	highlight_groups = vim.list_extend(
-		require("markview").configuration.highlight_groups,
-		require("markview.presets").heading.label_hls
-	),
-
-	headings = require("markview.presets").heading.label,
-
 	buf_ignore = { "nofile" },
 	modes = { "n" },
 
@@ -14,12 +7,3 @@ require("markview").setup({
 		hl = "dark",
 	},
 })
-
--- require("render-markdown").setup({
--- 	file_types = { "markdown", "vimwiki" },
--- 	highlights = {
--- 		code = "markdownCodeBlock",
--- 	},
--- })
-
--- vim.api.nvim_set_hl(0, "markdownCodeBlock", { bg = "none" })
