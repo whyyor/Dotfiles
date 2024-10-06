9180ae472507d292970cd580f6f35407557aee11
whyyor <whyyor@gmail.com>
2024-10-06 22:02:45 +0530

    UPDATE: disable smooth scrolling

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -87,13 +87,13 @@ require("lazy").setup({
 	},
 
 	-- Add smooth scrolling to avoid jumps
-	{
-		"karb94/neoscroll.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("neoscroll").setup()
-		end,
-	},
+	-- {
+	-- 	"karb94/neoscroll.nvim",
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require("neoscroll").setup()
+	-- 	end,
+	-- },
 
 	-- All closing buffers without closing the split
 	{
@@ -321,6 +321,22 @@ require("lazy").setup({
 
 	-- Dressing - for code actions
 	{ "stevearc/dressing.nvim", event = "VeryLazy" },
+
+	--- Flash
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	  },
+	},
 
 	----------------------------- Languages ---------------------------------
 
