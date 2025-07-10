73d97729b65c9282d1063bc727b5106789678819
whyyor <whyyor@gmail.com>
2025-07-10 13:33:49 +0530

    UPDATE: telescope to only show current dir files in history

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -71,7 +71,9 @@ telescope.load_extension("ui-select")
 map("n", "<leader>f", builtin.find_files, { desc = "Find files" })
 map("n", "<leader>b", builtin.buffers, { desc = "Show buffers" })
 map("n", "<leader>g", builtin.live_grep, { desc = "Live grep" })
-map("n", "<leader>h", builtin.oldfiles, { desc = "File history" })
+map("n", "<leader>h", function()
+	builtin.oldfiles({ cwd_only = true })
+end, { desc = "File history" })
 map(
 	"n",
 	"<leader>F",
